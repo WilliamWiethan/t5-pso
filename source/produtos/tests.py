f7ef18ffc43b0dde6d2aaeed3734e5527205482b
williamwiethan <williamswiethan@gmail.com>
2019-06-19 11:40:30 -0300

    .

--- source/produtos/tests.py
+++ source/produtos/tests.py
@@ -1,24 +1,24 @@
 from django.test import TestCase
 from django.http import HttpRequest
-from django.test import SimpleTestCase
+#from django.test import SimpleTestCase
 from django.urls import reverse
 
 from . import views
 
-class HomePageTests(SimpleTestCase):
+class HomePageTests(TestCase):
 
     def test_home_page_status_code(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
 
     def test_view_url_by_name(self):
-        response = self.client.get(reverse('home'))
+        response = self.client.get(reverse('produtos'))
         self.assertEquals(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('home'))
+        response = self.client.get(reverse('produtos'))
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'produtos.html')
 
     def test_home_page_contains_correct_html(self):
         response = self.client.get('/')
