39673b9edb193025a111eb42c540bae45659da6c
williamwiethan <williamswiethan@gmail.com>
2019-06-19 14:05:40 -0300

    .

--- source/test_deploy.py
+++ source/test_deploy.py
@@ -1,16 +1,10 @@
-import sys
-import os
-#import imp
-
 import pytest
 
 import server.settings as config
     
 def test_staticfiles():
-    #imp.reload(config)
     assert config.STATIC_URL == '/static/'
     assert 'whitenoise' in config.MIDDLEWARE[1].lower()
     
 def test_allowed_hosts():
-    #imp.reload(config)
     assert config.ALLOWED_HOSTS == ['*']
