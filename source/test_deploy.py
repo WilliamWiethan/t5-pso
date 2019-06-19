91dcba1bd33306bbd6877e2dfec50fd2d46951ed
williamwiethan <williamswiethan@gmail.com>
2019-06-19 11:34:38 -0300

    .

--- source/test_deploy.py
+++ source/test_deploy.py
@@ -1,20 +1,24 @@
 import sys
 import os
-import imp
+#import imp
 
 import pytest
 
 import server.settings as config
+import produtos.tests as testProd
     
 def test_staticfiles():
     #imp.reload(config)
-    
     assert config.STATIC_URL == '/static/'
     assert 'whitenoise' in config.MIDDLEWARE[1].lower()
     
-
 def test_allowed_hosts():
     #imp.reload(config)
-    
     assert config.ALLOWED_HOSTS == ['*']
-    
+
+def test_produtos():
+    assert testProd.HomePageTests.test_home_page_status_code == 200
+    assert testProd.HomePageTests.test_view_url_by_name == 200
+    assert testProd.HomePageTests.test_view_uses_correct_template == 200
+    assert testProd.HomePageTests.test_home_page_contains_correct_html
+    assert testProd.HomePageTests.test_home_page_does_not_contain_incorrect_html
